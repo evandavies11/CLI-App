4b8772497f769a26981bb173e26fa44fa29356d2
evandavies11 <evandavies11@gmail.com>
2019-08-10 19:08:09 -0500

    begin

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
-var keys = require("./keys.js");
-
 require('dotenv').config()
+
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment");
+
+var useApp = process.argv[2];
+
+var useSearch = process.argv.slice(3).join(" ";)
